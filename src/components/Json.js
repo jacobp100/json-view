066cdf3678bcbc56b6c1597ce9e2de6162cacdfe
Jacob Parker <jacobparker1992@gmail.com>
2016-05-07 11:29:57 +0100

    Escape HTML for strings

--- src/components/Json.js
+++ src/components/Json.js
@@ -12,6 +12,20 @@ const objectStart = '<span class="object"><a class="start">{</a>';
 const objectEnd = '<span class="end">}</span></span>';
 const abruptEnd = '</span>';
 
+const entityMap = {
+  '&': '&amp;',
+  '<': '&lt;',
+  '>': '&gt;',
+  '"': '&quot;',
+  "'": '&#39;',
+  '/': '&#x2F;',
+  '`': '&#x60;',
+  '=': '&#x3D;',
+};
+
+// Taken from mustache (so I assume it's good)
+const escapeHtml = (string) => String(string).replace(/[&<>"'`=\/]/g, s => entityMap[s]);
+
 const wrapContents = (elementsHtml, placeholder) =>
   `<span class="contents">${elementsHtml}</span><span class="collapse">${placeholder}</span>`;
 
@@ -25,10 +39,10 @@ const renderObjectEntry = (objectKeys) => ([key, value]) => {
 };
 
 const renderTypes = {
+  string: match => `<span class="string">${escapeHtml(match)}</span>`,
   number: match => `<span class="number">${match}</span>`,
-  string: match => `<span class="string">${match}</span>`,
   boolean: match => `<span class="boolean">${match}</span>`,
-  null: match => `<span class="null">${match}</span>`,
+  null: () => '<span class="null">null</span>',
   array(match, value, isComplete) {
     const elementsHtml = valueStart + value.map(render).join(valueSeparator) + valueEnd;
     const contents = value.length ? wrapContents(elementsHtml, value.length) : '';
